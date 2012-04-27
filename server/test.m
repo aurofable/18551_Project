04c69bd743ec70627566c79c092d0411642098af
James Chun <aurofable@gmail.com>
2012-04-27 05:48:39 -0400

    Changed code to take into account different labelling ranges

--- server/test.m
+++ server/test.m
@@ -13,11 +13,11 @@ numTestSamples = 200;
 % Preprocessing data
 testLabels = ones(numTestSamples * m, 1);
 for i = 1:m
-    testLabels((i-1)*numTestSamples+1:i*numTestSamples) = i*ones(numTestSamples, 1);
+    testLabels((i-1)*numTestSamples+1:i*numTestSamples) = labelVec(i)*ones(numTestSamples, 1);
 end
 
 % Creating the test data
-testData = dimRedTest(imgDataTestSeg, imgDataTestSegGray, dim, numTestSamples, m, reducFact, nVecs, rowDiv, colDiv, nHarmonics);
+testData = dimRedTest(imgDataTestSeg, imgDataTestSegGray, labelVec, dim, numTestSamples, m, reducFact, nVecs, rowDiv, colDiv, nHarmonics);
 testData = (testData - repmat(minimums, size(testData, 1), 1)) ./ repmat(ranges, size(testData, 1), 1);
 
 % Running the Test
