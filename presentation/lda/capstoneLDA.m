b6e53b12ea3e15cf9a557934ef850f4a0fde0f0a
James Chun <jtchun@andrew.cmu.edu>
2012-04-29 20:01:27 -0400

    Added lda scripts

--- presentation/lda/capstoneLDA.m
+++ presentation/lda/capstoneLDA.m
@@ -7,17 +7,25 @@ function res = capstoneLDA(data, labels)
 dataSize = size(data);
 classes = unique(labels);
 numClasses = length(classes);
+res = zeros(numClasses, dataSize(2) + 1);
 
 % Histogram of classes
 histClasses = zeros(numClasses, 1);
+classMean = zeros(numClasses, dataSize(2));
+covMat = zeros(dataSize(2), dataSize(2));
 for i = 1:numClasses
-    histClasses(i) = sum(labels == classes(i));
+    classData = (labels == classes(i));
+    histClasses(i) = sum(classData);
+    classMean(i,:) = mean(data(Group,:));
+    covMat = covMat + ((histClasses(i)-1)/(dataSize(1)-numClasses)).* cov(data(classData,:));
 end
 
-% Calculating the mean vector
+sampleProb = histClasses/n;
 
-
-
-
-
+% Loop over classes to calculate linear discriminant coefficients
+for i = 1:k,
+    Temp = classMean(i,:) / covMat;
+    res(i,1) = -0.5 * Temp * classMean(i,:)' + log(sampleProb(i));
+    res(i,2:end) = Temp;
+end
 end
